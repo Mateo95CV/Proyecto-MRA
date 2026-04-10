830e139b59fdfdc22431a7e5d7d80e2d186eadd6
Mateo95CV <dmcastanew@gmail.com>
2026-04-10 10:19:37 -0500

    Se quita useEffect para que funcione el despliegue en vercel

--- proyecto-mra/src/pages/ResetPassword.tsx
+++ proyecto-mra/src/pages/ResetPassword.tsx
@@ -1,5 +1,5 @@
 // src/pages/ResetPassword.tsx
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { supabase } from '../lib/supabaseClient';
 
